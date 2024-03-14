e8f08103da86cd106283ae779d9dab653b689745
Salu461 <131761608+Salu461@users.noreply.github.com>
2024-03-14 12:59:08 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const stagger = document.querySelectorAll(".stagger");
 const modal = document.querySelector(".modal");
 const toRight = document.querySelectorAll(".to-right");
+const article = document.querySelector(".wrap");
 
 gsap.from(stagger, {
   x: 100,
@@ -36,3 +37,21 @@ gsap.to(modal, {
   display: "none",
   delay: 3.5,
 });
+
+function secCall(entries) {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    gsap.to(article, {
+      opacity: 1,
+      duration: 0.5,
+    });
+  }
+}
+
+const sec1Obs = new IntersectionObserver(secCall, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-200px",
+});
+
+sec1Obs.observe(article);
